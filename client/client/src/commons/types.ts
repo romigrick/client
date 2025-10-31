4257332cb7fc76c709e581cdbbc09293216f992b
rickromig <richardsonromig201101@gmail.com>
2025-10-30 23:37:16 -0300

    adicionado interfaces e services para tratamento de produtos e categorias

--- client/client/src/commons/types.ts
+++ client/client/src/commons/types.ts
@@ -1,4 +1,3 @@
-
 export interface IUserRegister {
   displayName: string;
   username: string;
@@ -30,4 +29,19 @@ export interface AuthenticatedUser {
 export interface AuthenticationResponse {
   token: string;
   user: AuthenticatedUser;
+}
+
+export interface ICategory {
+  id?: number;
+  name: string;
+}
+
+export interface IProduct {
+  id?: number;
+  name: string;
+  description: string;
+  price: number;
+  category: ICategory;
+  imageName?: string;
+  contentType?: string;
 }
